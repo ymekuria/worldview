3c3bcd829257aa2d84de4eadab4081e10e661d58
Yoni <ymekuria@gmail.com>
2019-04-28 11:03:39 -0700

    add equirectangle scene

--- src/components/App.js
+++ src/components/App.js
@@ -4,28 +4,22 @@ import 'aframe';
 function App() {
   return (
     <a-scene>
-      <a-entity
-        geometry="primitive: box"
-        position="-1 0.5 -3"
-        rotation="0 45 0"
-        material="color: #4CC3D9"
-      />
-      <a-entity
-        geometry="primitive: sphere; radius: 1.25;"
-        position="0 1.25 -5"
-        material="color: #EF2D5E"
-      />
-      <a-entity
-        geometry="primitive: cylinder; radius: 0.5, height: 1.5"
+      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" />
+      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" />
+      <a-cylinder
         position="1 0.75 -3"
-        material="color: #FFC65D"
+        radius="0.5"
+        height="1.5"
+        color="#FFC65D"
       />
-      <a-entity
-        geometry="primitive: plane; width: 4; height: 4"
+      <a-plane
         position="0 0 -4"
         rotation="-90 0 0"
-        material="color: #7BC8A4"
+        width="4"
+        height="4"
+        color="#7BC8A4"
       />
+      <a-sky src={require('../teton.jpg')} />
     </a-scene>
   );
 }
